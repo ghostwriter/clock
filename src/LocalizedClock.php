db49b88017dd8ab44fa5306e07cdfcc1a4459d87
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-28 20:33:41 -0500

    Create LocalizedClock.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/LocalizedClock.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Clock;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use Ghostwriter\Clock\Interface\LocalizedClockInterface;
+use Ghostwriter\Clock\Trait\ClockTrait;
+
+/**
+ * A clock that returns the current time in a given timezone.
+ *
+ * @see \Ghostwriter\Clock\Tests\Unit\LocalizedClockTest
+ *
+ * @uses ClockTrait
+ *
+ * @immutable
+ */
+final readonly class LocalizedClock implements LocalizedClockInterface
+{
+    use ClockTrait;
+
+    public function __construct(
+        private readonly DateTimeZone $timezone = new DateTimeZone('UTC')
+    ) {
+    }
+
+    /**
+     * Returns the current time as a DateTimeImmutable Object.
+     */
+    public function now(): DateTimeImmutable
+    {
+        return new DateTimeImmutable('now', $this->timezone);
+    }
+}
