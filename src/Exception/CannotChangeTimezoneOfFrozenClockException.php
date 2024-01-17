4c72424202efadb4dc1b24425a36b9cf4155b30e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-17 08:34:04 -0600

    Update CannotChangeTimezoneOfFrozenClockException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/CannotChangeTimezoneOfFrozenClockException.php
+++ src/Exception/CannotChangeTimezoneOfFrozenClockException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Clock\Exception;
 
-use Ghostwriter\Clock\Interface\ExceptionInterface;
 use BadMethodCallException;
+use Ghostwriter\Clock\Interface\ClockExceptionInterface;
 
-final class CannotChangeTimezoneOfFrozenClockException extends BadMethodCallException implements ExceptionInterface
+final class CannotChangeTimezoneOfFrozenClockException extends BadMethodCallException implements ClockExceptionInterface
 {
 }
