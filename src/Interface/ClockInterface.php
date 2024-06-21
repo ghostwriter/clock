2e88de59fb2db5ea611c8c2839e734915628341f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-21 18:42:36 -0500

    Update ClockInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/ClockInterface.php
+++ src/Interface/ClockInterface.php
@@ -5,20 +5,10 @@ declare(strict_types=1);
 namespace Ghostwriter\Clock\Interface;
 
 use DateTimeImmutable;
-use DateTimeZone;
 
 interface ClockInterface
 {
     public function freeze(): FrozenClockInterface;
 
     public function now(): DateTimeImmutable;
-
-    public function withDateTimeZone(DateTimeZone $dateTimeZone): LocalizedClockInterface;
-
-    public function withSystemTimezone(): SystemClockInterface;
-
-    /**
-     * @param non-empty-string $timezone
-     */
-    public function withTimezone(string $timezone): LocalizedClockInterface;
 }
